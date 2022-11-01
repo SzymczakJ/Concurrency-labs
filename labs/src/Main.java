6b8b9d1c2d803d958de0482b7edb8d6601c2f2ac
SzymczakJ <szymczak.jakub12@gmail.com>
2022-11-01 22:05:36 +0100

    lab3v2

--- labs/src/Main.java
+++ labs/src/Main.java
@@ -38,6 +38,5 @@ public class Main {
         System.out.println(sut.variable);
     }
 }
-//testv2
 // Dwóch producentów (P1 i P2) i konsument(K1). 1. Wchodzi P1 i produkuje(B = 1). 2. Wchodzi P2 i próbuje produkować, więc waituje(B = 1). 3. Wchodzi K1 i zjada(B = 0). 4. Wchodzi
 //Dwóch producentów (P1 i P2) i konsument(K1). 1. Wchodzi P1 i produkuje(B = 1). 2 Wchodzi P2 i próbuje produkować, więc waituje(B = 1). P1 znowu się blokuje. Konsument zmienia bufor na zero i budzi P2 i obaj są na locku. Wchodzi K1 i waituje. P1 produkuje i wybudza P2. P1 waituje i P2 waituje.
